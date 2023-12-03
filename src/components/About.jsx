f7fb511d9f0015d1d6af10c776987ab91ba8c3af
Deep Patel <deeppatel7521@gmail.com>
2023-12-03 15:40:53 +0530

    Added Home, NavBar, About, SocialLinks component

---
+++ src/components/About.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className=" max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full ">
+        <div className=" pb-8">
+          <p className=" text-4xl  font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+
+        <p className="text-xl mt-20">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem ipsa
+          voluptate maiores culpa in harum exercitationem dignissimos ullam,
+          eveniet animi nihil quibusdam nesciunt deserunt ut quos porro optio
+          excepturi illo illum dolorum! Voluptate ut dolore velit cumque
+          corporis distinctio quod commodi ipsam incidunt voluptatum ex sint,
+          sit blanditiis voluptates, iste temporibus ipsa cum voluptatibus.
+          Itaque atque reiciendis quia rem praesentium! Recusandae
+          exercitationem nostrum enim adipisci, iusto veritatis voluptate quis
+          quas?
+        </p>
+
+        <br />
+
+        <p className=" text-xl">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Necessitatibus perferendis, voluptatum facilis repellendus nesciunt
+          beatae cumque officia commodi eos eius iure dolorem earum itaque quas
+          quibusdam dolorum accusamus similique explicabo quos molestias
+          pariatur qui. Debitis rem ipsa aliquam laborum incidunt praesentium
+          saepe recusandae quis distinctio modi dicta tempora, inventore
+          temporibus atque odit ducimus? Iste officia, excepturi natus minima
+          sequi, incidunt obcaecati, quod vitae earum corporis praesentium
+          deserunt. Officia, sunt error.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
