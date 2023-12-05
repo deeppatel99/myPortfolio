29ab129618fa21dd785b5b5604b438aa994e3a23
Deep Patel <deeppatel7521@gmail.com>
2023-12-05 19:34:36 +0530

    Added remaing component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,40 +3,34 @@ import React from "react";
 const About = () => {
   return (
     <div
-      name="about"
+      name="About"
       className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
     >
-      <div className=" max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full ">
-        <div className=" pb-8">
-          <p className=" text-4xl  font-bold inline border-b-4 border-gray-500">
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             About
           </p>
         </div>
 
         <p className="text-xl mt-20">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem ipsa
-          voluptate maiores culpa in harum exercitationem dignissimos ullam,
-          eveniet animi nihil quibusdam nesciunt deserunt ut quos porro optio
-          excepturi illo illum dolorum! Voluptate ut dolore velit cumque
-          corporis distinctio quod commodi ipsam incidunt voluptatum ex sint,
-          sit blanditiis voluptates, iste temporibus ipsa cum voluptatibus.
-          Itaque atque reiciendis quia rem praesentium! Recusandae
-          exercitationem nostrum enim adipisci, iusto veritatis voluptate quis
-          quas?
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius,
+          deserunt illum mollitia officiis qui exercitationem perferendis neque
+          quasi a recusandae necessitatibus tempora iusto! Blanditiis error
+          iste, totam fugiat recusandae rerum laborum perferendis molestiae
+          aperiam asperiores nemo. Magni dolor maxime debitis vitae, eaque hic
+          ab mollitia voluptatibus, a nostrum eveniet laborum!
         </p>
 
         <br />
 
-        <p className=" text-xl">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Necessitatibus perferendis, voluptatum facilis repellendus nesciunt
-          beatae cumque officia commodi eos eius iure dolorem earum itaque quas
-          quibusdam dolorum accusamus similique explicabo quos molestias
-          pariatur qui. Debitis rem ipsa aliquam laborum incidunt praesentium
-          saepe recusandae quis distinctio modi dicta tempora, inventore
-          temporibus atque odit ducimus? Iste officia, excepturi natus minima
-          sequi, incidunt obcaecati, quod vitae earum corporis praesentium
-          deserunt. Officia, sunt error.
+        <p className="text-xl">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum
+          pariatur, vel similique sint, nobis aspernatur ut praesentium
+          explicabo ipsam aliquid quasi laboriosam et culpa possimus repudiandae
+          quisquam ullam maiores ab unde. Fugiat odio mollitia nemo alias.
+          Commodi facilis atque nulla vero voluptatem explicabo. Quibusdam,
+          magni quo! Eum cupiditate debitis labore.
         </p>
       </div>
     </div>
